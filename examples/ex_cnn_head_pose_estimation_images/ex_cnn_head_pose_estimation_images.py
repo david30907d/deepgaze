a927c86eea734bb063f289bb60eeade897e17022
david <davidtnfsh@gmail.com>
2018-09-09 12:37:55 +0800

    now we can do head pose estimation with customary pic

--- examples/ex_cnn_head_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
+++ examples/ex_cnn_head_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
@@ -11,7 +11,13 @@
 import os
 import tensorflow as tf
 import cv2
+import tqdm
+import json
 from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
+from pathlib import Path
+import face_recognition
+from keras.preprocessing import image as keras_image
+from PIL import Image
 
 sess = tf.Session() #Launch the graph in a session.
 my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
@@ -20,14 +26,40 @@ my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
 my_head_pose_estimator.load_roll_variables(os.path.realpath("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
 my_head_pose_estimator.load_pitch_variables(os.path.realpath("../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
 my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
+img_dict = {}
 
-for i in range(1,9):
-    file_name = str(i) + ".jpg"
+def get_head_pose_estimation(file_name):
+    origin_image_numpy = face_recognition.load_image_file(file_name)
+    # See also: find_faces_in_picture_cnn.py
+    face_locations = face_recognition.face_locations(origin_image_numpy, model='cnn')
+    if len(face_locations) != 1:
+        # means there are multiple faces in a picture
+        # discard this picture
+        return None
+    else:
+        face_location = face_locations[0]
+    # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    # You can access the actual face itself like this:
+    face_image_numpy = origin_image_numpy[top:bottom, left:right]
+    face_image_pil = Image.fromarray(face_image_numpy).resize((100, 100), Image.BILINEAR)
+    face_image_numpy = keras_image.img_to_array(face_image_pil)
     print("Processing image ..... " + file_name)
-    image = cv2.imread(file_name) #Read the image with OpenCV
+    # image = cv2.imread(file_name) #Read the image with OpenCV
     # Get the angles for roll, pitch and yaw
-    roll = my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle using a CNN
-    pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
-    yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
-    print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
-    print("")
+    roll = my_head_pose_estimator.return_roll(face_image_numpy)  # Evaluate the roll angle using a CNN
+    # pitch = my_head_pose_estimator.return_pitch(face_image_numpy)  # Evaluate the pitch angle using a CNN
+    yaw = my_head_pose_estimator.return_yaw(face_image_numpy)
+    return [roll[0,0,0], yaw[0,0,0]]
+
+def cal_sim_of_folder(folder_name):
+    for file_name in tqdm.tqdm(Path(folder_name).iterdir()):
+        file_name = str(file_name)
+        head_pose = get_head_pose_estimation(file_name)
+        if head_pose:
+            img_dict[file_name] = head_pose
+
+if __name__ == '__main__':
+    cal_sim_of_folder('porn')
+    cal_sim_of_folder('林志玲')
+    json.dump(img_dict, open('pic_similarity.json', 'w'))
